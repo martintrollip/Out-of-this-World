cfeba75aa378eae335c467a7fa706b658d08f950
Martin <martin@Martins-iMac.local>
2016-04-24 16:55:38 +0200

    Add object to list

---
+++ Out of this World/AddSpaceObjectViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddSpaceObjectViewController.h
+//  Out of this World
+//
+//  Created by Martin on 2016/04/24.
+//  Copyright © 2016 Martin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddSpaceObjectViewController : UIViewController
+
+@end
---
+++ Out of this World/AddSpaceObjectViewController.m
@@ -0,0 +1,37 @@
+//
+//  AddSpaceObjectViewController.m
+//  Out of this World
+//
+//  Created by Martin on 2016/04/24.
+//  Copyright © 2016 Martin. All rights reserved.
+//
+
+#import "AddSpaceObjectViewController.h"
+
+@interface AddSpaceObjectViewController ()
+
+@end
+
+@implementation AddSpaceObjectViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
